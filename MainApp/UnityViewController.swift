ffcbff8fa1f4479bdf07a3a4f8e8c3b765e10494
Jose Aponte <japonte@tappsi.co>
2017-08-29 11:38:16 -0500

    Added navigation controller and push unity game in another view controller

--- MainApp/UnityViewController.swift
+++ MainApp/UnityViewController.swift
@@ -6,5 +6,37 @@
 //  Copyright © 2017 Tappsi. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
+class UnityViewController: UIViewController {
+    
+    var unityView: UIView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.startUnity()
+        
+        unityView = UnityGetGLView()!
+        
+        self.view!.addSubview(unityView!)
+        unityView!.translatesAutoresizingMaskIntoConstraints = false
+        
+        // look, non-full screen unity content!
+//        let views = ["view": unityView]
+//        let w = NSLayoutConstraint.constraints(withVisualFormat: "|-20-[view]-20-|", options: [], metrics: nil, views: views)
+//        let h = NSLayoutConstraint.constraints(withVisualFormat: "V:|-75-[view]-50-|", options: [], metrics: nil, views: views)
+        //view.addConstraints(w + h)
+        
+    }
+    
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.stopUnity()
+        unityView!.removeFromSuperview()
+    }
+    
+}
--- MainApp/ViewController.swift
+++ MainApp/ViewController.swift
@@ -12,28 +12,28 @@ class ViewController: UIViewController {
     var unityView: UIView?
     
     @IBAction func start(_ sender: Any) {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        appDelegate.startUnity()
-        
-        unityView = UnityGetGLView()!
-        
-        self.view!.addSubview(unityView!)
-        unityView!.translatesAutoresizingMaskIntoConstraints = false
-        
-        // look, non-full screen unity content!
-        let views = ["view": unityView]
-        let w = NSLayoutConstraint.constraints(withVisualFormat: "|-20-[view]-20-|", options: [], metrics: nil, views: views)
-        let h = NSLayoutConstraint.constraints(withVisualFormat: "V:|-75-[view]-50-|", options: [], metrics: nil, views: views)
-        view.addConstraints(w + h)
-        
-        view.addSubview(unityView!)
+//        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+//        appDelegate.startUnity()
+//        
+//        unityView = UnityGetGLView()!
+//        
+//        self.view!.addSubview(unityView!)
+//        unityView!.translatesAutoresizingMaskIntoConstraints = false
+//        
+//        // look, non-full screen unity content!
+//        let views = ["view": unityView]
+//        let w = NSLayoutConstraint.constraints(withVisualFormat: "|-20-[view]-20-|", options: [], metrics: nil, views: views)
+//        let h = NSLayoutConstraint.constraints(withVisualFormat: "V:|-75-[view]-50-|", options: [], metrics: nil, views: views)
+//        view.addConstraints(w + h)
+//        
+//        view.addSubview(unityView!)
     }
 
 
     @IBAction func stop(_ sender: Any) {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        appDelegate.stopUnity()
-        unityView!.removeFromSuperview()
+//        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+//        appDelegate.stopUnity()
+//        unityView!.removeFromSuperview()
     }
     
 }
